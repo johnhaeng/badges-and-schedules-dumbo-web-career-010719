7a4c533df6f3d26603b8311a72b03e12c3ac5737
Jun Haeng Lee <johnhaeng@gmail.com>
2018-12-13 21:59:31 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+
+
+def badge_maker(person_name)
+  "Hello, my name is #{person_name}."
+end
+
+def batch_badge_creator(name_array)
+  badge_array = []
+  
+  name_array.each do |name|
+    badge_array.push(badge_maker(name))
+  end
+  
+  badge_array
+end
+
+def assign_rooms(list_of_speakers)
+  new_array = []
+  
+  list_of_speakers.each_with_index do |name, index|
+    new_array.push("Hello, #{name}! You'll be assigned to room #{index+1}!")
+  end
+  
+  new_array
+end
+
+def printer(names)
+  batch_badge_creator(names)
+  assign_rooms(names)
+end
